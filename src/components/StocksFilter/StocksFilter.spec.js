441ad3961b235fbad91e95904da8c72aecbcc00b
Waseem Ahmad <waseem.ahmad@waitrose.co.uk>
2021-10-20 11:40:10 +0100

    mock_test_example

--- src/components/StocksFilter/StocksFilter.spec.js
+++ src/components/StocksFilter/StocksFilter.spec.js
@@ -2,14 +2,14 @@ import { shallow } from "enzyme";
 import React from "react";
 import List from "./index.js";
 
-const setup = (props ={}) => {
-    return shallow(<List {...props} />)
-}
+const setup = (props = {}) => {
+  return shallow(<List {...props} />);
+};
 
 describe("Stocks Filter Compoenent", () => {
-//   let wrapper;
+  //   let wrapper;
 
-//   wrapper = shallow(<List />);
+  //   wrapper = shallow(<List />);
 
   test("renders without error", () => {});
 });
---
+++ src/components/mock_test_example.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+describe ('state of controlled input field', () => {
+    test ('state update with value of input box upon change', () => {
+    const mockGuess = jest.fn();
+
+//Using destructure
+jest.mock ('react', () => ({
+    ...jest.requireActual('react'),
+    useState: (initailState) => [initialState, mockGuess]
+
+}))
+
+
+
+//Without using destructure
+    React.useState = jest.fn(() => ["", mockGuess]);
+    const wrapper = setup();
+    const inputBox = (wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate("change", mockevent);
+
+    expect (mockGuess).toHaveBeenCalledWith('train');
+});
+
+});
